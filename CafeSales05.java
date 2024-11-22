2816f8028bb52f555dd34a0e03ff22dacc69b68b
CitraAyu0 <meilindacitraayu@gmail.com>
2024-11-22 12:05:09 +0700

    tugas 3

--- CafeSales05.java
+++ CafeSales05.java
@@ -1,16 +1,34 @@
+import java.util.Scanner;
+
 public class CafeSales05 {
 
-    static int[][] penjualan = {
-        {20, 20, 25, 20, 10, 60, 10}, 
-        {30, 80, 40, 10, 15, 20, 25}, 
-        {5, 9, 20, 25, 10, 5, 45},   
-        {50, 8, 17, 18, 10, 30, 6},   
-        {15, 10, 16, 15, 10, 10, 55}  
-    };
-
-    static String[] menu = {"Kopi", "Teh", "Es Degan", "Roti Bakar", "Gorengan"};
+    static int[][] penjualan;
+    static String[] menu;
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah menu: ");
+        int jumlahMenu = input.nextInt();
+        menu = new String[jumlahMenu];
+
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.print("Masukkan nama menu ke-" + (i + 1) + ": ");
+            menu[i] = input.next();
+        }
+
+        System.out.print("Masukkan jumlah hari penjualan: ");
+        int jumlahHari = input.nextInt();
+        penjualan = new int[jumlahMenu][jumlahHari];
+
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.println("Masukkan data penjualan untuk menu " + menu[i] + ":");
+            for (int j = 0; j < jumlahHari; j++) {
+                System.out.print("  Hari ke-" + (j + 1) + ": ");
+                penjualan[i][j] = input.nextInt();
+            }
+        }
+
         SeluruhDataPenjualan();
 
         MenuPenjualanTertinggi();
@@ -19,7 +37,7 @@ public class CafeSales05 {
     }
 
     public static void SeluruhDataPenjualan() {
-        System.out.println("Data Penjualan:");
+        System.out.println("\nData Penjualan:");
         for (int i = 0; i < penjualan.length; i++) {
             System.out.print(menu[i] + ": ");
             for (int j = 0; j < penjualan[i].length; j++) {
@@ -30,21 +48,21 @@ public class CafeSales05 {
     }
 
     public static void MenuPenjualanTertinggi() {
-        int PenjualanTertinggi = 0;
-        String MenuTertinggi = "";
+        int penjualanTertinggi = 0;
+        String menuTertinggi = "";
 
         for (int i = 0; i < penjualan.length; i++) {
             int total = 0;
             for (int j = 0; j < penjualan[i].length; j++) {
                 total += penjualan[i][j];
             }
-            if (total > PenjualanTertinggi) {
-                PenjualanTertinggi = total;
-                MenuTertinggi = menu[i];
+            if (total > penjualanTertinggi) {
+                penjualanTertinggi = total;
+                menuTertinggi = menu[i];
             }
         }
 
-        System.out.println("\nMenu dengan penjualan tertinggi: " + PenjualanTertinggi + " (Total: " + PenjualanTertinggi + ")");
+        System.out.println("\nMenu dengan penjualan tertinggi: " + menuTertinggi + " (Total: " + penjualanTertinggi + ")");
     }
 
     public static void Rata2Penjualan() {
